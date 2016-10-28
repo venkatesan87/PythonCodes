e6c4336a548d2ccc4c45c069e82240ae47a1035f
venkatesan87 <venkat.elex@gmail.com>
2016-10-28 12:39:16 -0700

    Update BillCalculator.py

--- BillCalculator.py
+++ BillCalculator.py
@@ -1,19 +1,17 @@
-'''A simple program to calculate final bill with tax and tips. Program part of a Codeacedmy course'''
-
 #fn to add tax to bill cost
 def tax(bill):
     """Adds 8% tax to a restaurant bill."""
     bill *= 1.08
-    print "With tax: %f" % bill  ## printed floating rather string %s
+    print "With 8 pct tax: %f" % bill  ## printed floating rather string %s
     return bill
 
 #fn to add tip to taxed bill cost
 def tip(bill):
     """Adds 15% tip to a restaurant bill."""
     bill *= 1.15
-    print "With tip: %f" % bill
+    print "With 15 pct tip: %f" % bill
     return bill
-    
-meal_cost = raw_input ('Enter meal cost')
+
+meal_cost = raw_input ('Enter meal cost ')
 meal_with_tax = tax(int(meal_cost)) ##converting string to int using int()
 meal_with_tip = tip(meal_with_tax)
